184ce50d568affb24fbaebb5f259aa9f7a81c44b
uenoaki <radaokuno2525@gmail.com>
2023-07-15 13:10:52 +0900

    Add some calc

--- src/reader.rs
+++ src/reader.rs
@@ -1,4 +1,5 @@
 use anyhow::{Ok, Result};
+use byteorder::{BigEndian, LittleEndian, ByteOrder};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -10,9 +11,18 @@ enum ReadError {
 }
 
 pub trait Read {
-    fn read_varint(&self, offset: u64)->Result<(u64,u64)>;
+    fn read_i32(&self, offset: u64) -> i32;
+    fn read_li32(&self, offset: u64) -> i32;
+    fn read_varint(&self, offset: u64) -> Result<(u64, u64)>;
+    fn read_little_string(&self, offset: u64) -> Result<(String, u64)>;
 }
 impl Read for Vec<u8> {
+    fn read_i32(&self, offset: u64) -> i32 {
+        BigEndian::read_i32(&self[(offset as usize)..])
+    }
+    fn read_li32(&self, offset: u64) -> i32 {
+        LittleEndian::read_i32(&self[(offset as usize)..])
+    }
     fn read_varint(&self, offset: u64)->Result<(u64,u64)> {
         let mut value: u64 = 0;
         let mut shift: u8 = 0;
@@ -29,5 +39,17 @@ impl Read for Vec<u8> {
             }
             shift += 7;
         }
+    }
+    fn read_little_string(&self, offset: u64) -> Result<(String, u64)> {
+        let mut cursor: u64 = offset;
+        let value = self.read_li32(offset) as u64;
+        cursor += 4;
+        if cursor + value > self.len() as u64 {
+            return Err(ReadError::MissingCharacters(self.len(), cursor + value).into());
+        }
+        let edge = cursor + value;
+        let str = String::from_utf8(self[(cursor as usize)..(edge as usize)].to_vec()).unwrap();
+        let size = 4 + value;
+        Ok((str, size))
     }
 }
--- src/writer.rs
+++ src/writer.rs
@@ -3,6 +3,8 @@ use byteorder::WriteBytesExt;
 
 pub trait Write {
     fn write_var_int(&mut self, value: u64) -> Result<u64>;
+    fn write_bool(&mut self, value: bool) -> Result<()>;
+    fn write_string(&mut self, value: String) -> Result<u64>;
 }
 impl Write for Vec<u8> {
     fn write_var_int(&mut self, value: u64) -> Result<u64> {
@@ -16,5 +18,16 @@ impl Write for Vec<u8> {
         self.write_u8(v as u8)?;
         cursor += 1;
         Ok(cursor)
+    }
+    fn write_bool(&mut self, value: bool) -> Result<()> {
+        self.write_i8(value.into())?;
+        Ok(())
+    }
+    fn write_string(&mut self, value: String) -> Result<u64> {
+        let mut cursor = 0;
+        let len = value.as_bytes().len() as u64;
+        cursor += self.write_var_int(len)?;
+        self.append(&mut value.as_bytes().to_vec());
+        Ok(cursor + len)
     }
 }
