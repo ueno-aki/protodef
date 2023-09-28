2debacee195dcbe1d1959c1f862086f249f3535e
uenoaki <radaokuno2525@gmail.com>
2023-09-28 11:27:53 +0900

    macros

--- src/reader.rs
+++ src/reader.rs
@@ -1,5 +1,4 @@
-use anyhow::{Ok, Result, anyhow};
-use byteorder::{BigEndian, LittleEndian, ByteOrder};
+use anyhow::Result;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -7,58 +6,88 @@ pub enum ReadError {
     #[error("Invalid index ,expected at most {0}")]
     OutOfBounds(usize),
     #[error("Missing characters in string, found size is {0}, expected size was {1}")]
-    MissingCharacters(usize, u64),
+    MissingCharacters(usize, usize),
     #[error("Failed to parse {0} into bool")]
     FailedIntoBoolean(u8)
 }
-pub trait Read {
-    fn read_i32(&self, offset: u64) -> i32;
-    fn read_li32(&self, offset: u64) -> i32;
-    fn read_varint(&self, offset: u64) -> Result<(u64, u64)>;
-    fn read_bool(&self, offset: u64) -> Result<(bool,u64)>;
-    fn read_little_string(&self, offset: u64) -> Result<(String, u64)>;
+macro_rules! native_reader {
+    ($($native:ty),*) => {
+        use paste::paste;
+        use byteorder::{BigEndian, LittleEndian, ByteOrder};
+        pub trait NativeReader {
+            $(
+                paste! {
+                    fn [<read_ $native>](&self, offset: usize) -> $native;
+                    fn [<read_l $native>](&self, offset: usize) -> $native;
+                }
+            )*
+            fn read_u8(&self, offset: usize) -> u8;
+        }
+        impl NativeReader for Vec<u8> {
+            $(
+                paste! {
+                    #[inline]
+                    fn [<read_ $native>](&self, offset: usize) -> $native {
+                        BigEndian::[<read_$native>](&self[offset..])
+                    }
+                    #[inline]
+                    fn [<read_l $native>](&self, offset: usize) -> $native {
+                        LittleEndian::[<read_$native>](&self[offset..])
+                    }
+                }
+            )*
+            #[inline]
+            fn read_u8(&self, offset: usize) -> u8 {
+                self[offset]
+            }
+        }
+    };
 }
-impl Read for Vec<u8> {
-    fn read_i32(&self, offset: u64) -> i32 {
-        BigEndian::read_i32(&self[(offset as usize)..])
-    }
-    fn read_li32(&self, offset: u64) -> i32 {
-        LittleEndian::read_i32(&self[(offset as usize)..])
-    }
-    fn read_varint(&self, offset: u64)->Result<(u64,u64)> {
+native_reader![u16,u32,u64,i16,i32,i64,f32,f64];
+
+pub trait ProtodefReader {
+    fn read_varint(&self, offset: usize) -> Result<(u64, usize)>;
+    fn read_bool(&self, offset: usize) -> Result<(bool,usize)>;
+    fn read_little_string(&self, offset: usize) -> Result<(String, usize)>;
+}
+impl ProtodefReader for Vec<u8> {
+    #[inline]
+    fn read_varint(&self, offset: usize)->Result<(u64,usize)> {
         let mut value: u64 = 0;
-        let mut shift: u8 = 0;
+        let mut shift: u64 = 0;
         let mut cursor = offset;
         loop {
-            if (cursor + 1) > self.len() as u64{
+            if (cursor + 1) > self.len(){
                 break Err(ReadError::OutOfBounds(self.len()).into());
             }
-            let byte = self[cursor as usize];
-            value |= (byte as u64 & 0x7f) << shift;
+            let byte = self[cursor] as u64;
+            value |= (byte & 0x7f) << shift;
             cursor += 1;
-            if (byte as u64 & 0x80) == 0 {
+            if (byte & 0x80) == 0 {
                 break Ok((value, cursor - offset));
             }
             shift += 7;
         }
     }
-    fn read_bool(&self, offset: u64) -> Result<(bool,u64)> {
-        match self[offset as usize] {
+    #[inline]
+    fn read_bool(&self, offset: usize) -> Result<(bool,usize)> {
+        match self[offset] {
             n if n == 0 => Ok((false,1)),
             n if n == 1 => Ok((true,1)),
-            _ => Err(ReadError::FailedIntoBoolean(self[offset as usize]).into())
+            _ => Err(ReadError::FailedIntoBoolean(self[offset]).into())
         }
     }
-    fn read_little_string(&self, offset: u64) -> Result<(String, u64)> {
-        let mut cursor: u64 = offset;
-        let value = self.read_li32(offset) as u64;
+    #[inline]
+    fn read_little_string(&self, offset: usize) -> Result<(String, usize)> {
+        let mut cursor = offset;
+        let str_size = self.read_li32(offset) as usize;
         cursor += 4;
-        if cursor + value > self.len() as u64 {
-            return Err(ReadError::MissingCharacters(self.len(), cursor + value).into());
+        let edge = cursor + str_size;
+        if edge > self.len() {
+            return Err(ReadError::MissingCharacters(self.len(), edge).into());
         }
-        let edge = cursor + value;
-        let str = String::from_utf8(self[(cursor as usize)..(edge as usize)].to_vec()).unwrap();
-        let size = 4 + value;
-        Ok((str, size))
+        let str = String::from_utf8(self[cursor..edge].to_vec())?;
+        cursor += str_size;
+        Ok((str, cursor))
     }
 }
